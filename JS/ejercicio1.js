8585a4a9ff4e980f1d8ea7187a8088d55e0e60b5
matiasroyann <matiasroyan11@gmail.com>
2024-05-10 13:01:02 -0300

    agregue los html y empece los ejs de la guai de funciones

---
+++ JS/ejercicio1.js
@@ -0,0 +1,34 @@
+//1
+function rectangulo(a,b){
+    return a*b
+}
+
+//2
+a
+function triangulo(a,b){
+    return a*b/2
+}
+
+//3
+let x =[1,2]
+function largoDelArray(x){
+    return x.length 
+}
+console.log(largoDelArray)
+
+//4
+let c = "hola"
+function cantidadDeLetras(c){
+    return c.length
+}   
+//5
+function dolarHoy(y){
+    return y/100
+
+}
+
+//6
+
+function precioFinal(p){
+    
+}

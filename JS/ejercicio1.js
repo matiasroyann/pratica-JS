08b5de47577d3a88dd28629863c36181afc624f2
matiasroyann <matiasroyan11@gmail.com>
2024-05-11 13:17:18 -0300

    agregue mas ejs a practica funciones

--- JS/ejercicio1.js
+++ JS/ejercicio1.js
@@ -1,29 +1,36 @@
+let a = 10 ;
+let b = 5 ;
+
 //1
-function rectangulo(a,b){
-    return a*b
+function rectangulo(a,b){ 
+    return a*b ;
 }
 
 //2
-a
+
 function triangulo(a,b){
-    return a*b/2
+    return a*b/2 ;
 }
 
 //3
-let x =[1,2]
-function largoDelArray(x){
-    return x.length 
+
+let x = [1,2] ;
+
+function largoDelArray(x){ 
+    return x.length ;
 }
-console.log(largoDelArray)
+
+console.log(largoDelArray) ;
 
 //4
-let c = "hola"
+let c = "hola" ;
 function cantidadDeLetras(c){
-    return c.length
+    return c.length ;
 }   
 //5
+let y = 1000 ;
 function dolarHoy(y){
-    return y/100
+    return y/100 ;
 
 }
 

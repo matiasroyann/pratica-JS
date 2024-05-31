bcb25e2db4e2e9b5e54d1140907ef07bbd9d0b94
matiasroyann <matiasroyan11@gmail.com>
2024-05-31 15:19:25 -0300

    termine practica apis

--- Practica-JS/JS/alertasycondicionales/precalentando.js
+++
@@ -1,7 +0,0 @@
-var preg = confirm("¿Vas a salir de tu casa?")
-
-if (preg) {
-    alert("Chequeá el pronóstico del tiempo antes de salir")
-} else {
-    alert("¡Qué bueno que te quedes! Va a llover mucho hoy.")
-}
